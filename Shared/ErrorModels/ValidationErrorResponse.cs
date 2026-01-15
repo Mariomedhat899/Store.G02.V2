ce2d91789f6e55aa0c0641b9de929aa5e2a2d2ae
Mariomedhat899 <mariomedhat899@gmail.com>
2026-01-15 04:10:03 +0200

    Validation Error handling WithModelState

---
+++ Shared/ErrorModels/ValidationErrorResponse.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.ErrorModels
+{
+    public class ValidationErrorResponse
+    {
+        public int StatusCode { get; set; } = StatusCodes.Status400BadRequest;
+        public string ErrorMessage { get; set; } = "Validation Errors !!";
+
+        public IEnumerable<ValdationError> Errors { get; set; }
+    }
+
+    public class ValdationError
+    {
+        public string FieldName { get; set; } 
+        public IEnumerable<string> Errors { get; set; } 
+
+    }
+}
