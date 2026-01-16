96dd34546c0812861d6d6fe356d1f8c863e29b33
Mariomedhat899 <mariomedhat899@gmail.com>
2026-01-16 04:50:14 +0200

    Main Refactoring

---
+++ Infrastructure/Periestence/InfrastructureServicesRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Periestence.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Periestence
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructeServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddScoped<IDbInitializer, DbInitializer>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+            return services;
+        }
+    }
+}
