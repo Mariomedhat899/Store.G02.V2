064ec2373cacbb3a424be3310c0688510af51e86
Mariomedhat899 <mariomedhat899@gmail.com>
2026-01-18 08:56:30 +0200

    Entites For Basket Module

---
+++ Core/Domain/Contracts/IBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entites.Baskets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public interface IBasketRepository
+    {
+      Task<CustomerBasket>  GetBasketAsync(string id);
+      Task<CustomerBasket>  CreateBasketAsync(CustomerBasket basket,TimeSpan duration);
+      Task<bool>  DeleteBasketAsync(string id);
+    }
+}
---
+++ Infrastructure/Periestence/Repositories/BasketRepository.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Contracts;
+using Domain.Entites.Baskets;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Periestence.Repositories
+{
+    public class BasketRepository(IConnectionMultiplexer connection) : IBasketRepository
+    {
+        private readonly IDatabase _redis = connection.GetDatabase();
+        public async Task<CustomerBasket> GetBasketAsync(string id)
+        {
+            var Redis= await _redis.StringGetAsync(id);
+
+            if (Redis.IsNullOrEmpty) return null;
+
+            var Basket = JsonSerializer.Deserialize<CustomerBasket>(Redis);
+
+            if(Basket is null) return null;
+            return Basket;
+            
+        }
+        public async Task<CustomerBasket> CreateBasketAsync(CustomerBasket basket, TimeSpan duration)
+        {
+            var RedisValue = JsonSerializer.Serialize(basket);
+          var flag = await _redis.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket),duration);
+
+            if (!flag) return null;
+
+            return await GetBasketAsync(basket.Id);
+
+        }
+
+        public Task<bool> DeleteBasketAsync(string id)
+        {
+          return  _redis.KeyDeleteAsync(id);
+        }
+
+    }
+}
