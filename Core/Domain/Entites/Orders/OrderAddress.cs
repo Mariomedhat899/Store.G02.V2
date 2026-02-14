d3d4e09b6072eb417c0d04ef30e0a8bdbcf9d6ee
Mario Medhat <118922155+Mariomedhat899@users.noreply.github.com>
2026-02-14 19:17:48 +0200

    Fix typo in LastName property

--- Core/Domain/Entites/Orders/OrderAddress.cs
+++ Core/Domain/Entites/Orders/OrderAddress.cs
@@ -3,7 +3,7 @@
     public class OrderAddress
     {
         public string FirstName { get; set; }
-        public string LasttName { get; set; }
+        public string LastName { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
